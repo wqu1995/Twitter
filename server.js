c1941fe521935db2f4455537310e2dc37cb7e61b
Wenjun Qu <wenjun.qu@stonybrook.edu>
2017-04-20 00:00:46 -0400

    bug2

--- server.js
+++ server.js
@@ -357,7 +357,7 @@ var postid = crypto.createHash('md5').update(req.body.content+cryptoRandomString
 					username: req.session.user,
 					timestamp: timestamp,
 					parent: req.body.parent,
-					media: req.body.media.toString()
+					media: "['"+req.body.media.toString()+"']"
 				}
 			}
 			else{
@@ -380,7 +380,7 @@ var postid = crypto.createHash('md5').update(req.body.content+cryptoRandomString
 					username: req.session.user,
 					timestamp: timestamp,
 					parent: null,
-					media: req.body.media.toString()
+					media: "['"+req.body.media.toString()+"']"
 				}
 			}
 			else{
@@ -1046,7 +1046,7 @@ app.delete('/item/:id',function(req,res){
 		}
 	})
 })*/
-console.log("in delete item")
+//console.log("in delete item")
 	connection.query('SELECT media FROM Tweets WHERE id = '+mysql.escape(req.params.id), function(err,result){
 		if(err){
 			res.send({
